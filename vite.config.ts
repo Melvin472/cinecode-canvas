e3d999de51d2354f5eeda653d243b64ee95edf9a
Melvin472 <melvin.lacote@gmail.com>
2026-02-17 18:42:06 +0100

    finalisatiopn

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/cinecode-canvas/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
